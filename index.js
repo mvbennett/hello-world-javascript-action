7b6a7b3dce0121227066af4078a121a9437dce0a
Mike Bennett <94943459+mvbennett@users.noreply.github.com>
2025-02-27 14:41:51 -0500

    bigger test

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
+const fs = require("fs");
+
+const onePath = "./one.js";
+const twoPath = "./two.json";
+const threePath = "./three.json";
+
+function getNum(path) {
+  const file = fs.readFileSync(path, "utf-8");
+  const number = JSON.parse(file)?.number;
+  return number;
+}
 
 try {
   // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput("who-to-greet");
-  console.log(`Hello ${nameToGreet}!`);
-  const time = new Date().toTimeString();
-  core.setFailed();
-  core.setOutput("time", time);
-  core.setOutput("stuff", true);
-  core.setOutput("things", true);
+  const oneNum = getNum(onePath);
+  const twoNum = getNum(twoPath);
+  const threeNum = getNum(threePath);
+  if ((oneNum == twoNum) == threeNum) {
+    core.setOutput("match", true);
+  } else {
+    throw new Error("no match!");
+  }
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
-  throw new Error("Wow this is sooooo bad");
 } catch (error) {
   core.setFailed(error.message);
 }
