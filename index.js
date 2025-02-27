731d6f078df3e704b772d25004a2d7f7894160ec
Mike Bennett <94943459+mvbennett@users.noreply.github.com>
2025-02-27 14:15:49 -0500

    intentionally throw error

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
-  core.setFailed(1);
+  throw new Error("Wow this is sooooo bad");
 } catch (error) {
   core.setFailed(error.message);
 }
