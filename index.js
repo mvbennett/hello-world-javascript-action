92c36cb3172a74e2db89046e29aa85bc296792f9
Mike Bennett <94943459+mvbennett@users.noreply.github.com>
2025-02-27 16:11:00 -0500

    found the mistake

--- index.js
+++ index.js
@@ -2,35 +2,34 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 const fs = require("fs");
 
-// const onePath = "./one.js";
-// const twoPath = "./two.json";
-// const threePath = "./three.json";
+const onePath = "./one.json";
+const twoPath = "./two.json";
+const threePath = "./three.json";
 
-// function getNum(path) {
-//   const file = fs.readFileSync(path, "utf-8");
-//   const number = JSON.parse(file)?.number;
-//   return number;
-// }
+function getNum(path) {
+  const file = fs.readFileSync(path, "utf-8");
+  const number = JSON.parse(file)?.number;
+  return number;
+}
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  // const oneNum = getNum(onePath);
-  // const twoNum = getNum(twoPath);
-  // const threeNum = getNum(threePath);
-  // if ((oneNum == twoNum) == threeNum) {
-  //   core.setOutput("match", "wow great job!");
-  // } else {
-  //   throw new Error("no match!");
-  // }
+  const oneNum = getNum(onePath);
+  const twoNum = getNum(twoPath);
+  const threeNum = getNum(threePath);
+  if ((oneNum == twoNum) == threeNum) {
+    core.setOutput("match", "wow great job!");
+  } else {
+    throw new Error("no match!");
+  }
   // // Get the JSON webhook payload for the event that triggered the workflow
   // const payload = JSON.stringify(github.context.payload, undefined, 2);
   // console.log(`The event payload: ${payload}`);
-  const one = true;
-  const two = true;
-  if (one != two) {
-    throw new Error("no match!");
-  }
-  core.setOutput("match", "wow great job");
+  // const one = true;
+  // const two = true;
+  // if (one != two) {
+  //   throw new Error("no match!");
+  // }
+  // core.setOutput("match", "wow great job");
 } catch (error) {
   core.setFailed(error.message);
 }
